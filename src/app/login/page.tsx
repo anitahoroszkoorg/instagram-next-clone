4bb6f8943e5d29a986501b23cf5eedd498c71880
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2023-07-31 20:20:59 +0200

    initial changes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>page where I will log in</div>;
+};
+
+export default page;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>page of things I will post</div>;
+};
+
+export default page;
