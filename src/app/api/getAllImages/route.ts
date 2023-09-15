4b605b4e334dbee8aca07fdf6eab6fcd2643dffd
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2023-09-15 21:22:25 +0200

    insert signed urls into database with prisma

--- src/app/api/getAllImages/route.ts
+++ src/app/api/getAllImages/route.ts
@@ -5,6 +5,26 @@ import {
   _Object,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const insertSignedUrls = async (signedUrls: string[]) => {
+  try {
+    for (const signedUrl of signedUrls) {
+      await prisma.post.create({
+        data: {
+          url: signedUrl,
+        },
+      });
+    }
+
+    console.log("Signed URLs inserted successfully.");
+  } catch (error) {
+    console.error("Error inserting signed URLs:", error);
+  } finally {
+    await prisma.$disconnect(); 
+  }
+};
 
 const client = new S3Client({});
 
