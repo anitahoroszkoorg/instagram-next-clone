9eb6afc0900796715addb2af2d45b00fb29d7fd8
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2023-09-19 20:28:04 +0200

    update components for creating posts in db

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,21 +2,20 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-//https://stackoverflow.com/questions/65746549/how-do-link-a-file-user-uploaded-via-pre-signed-url-to-database
-//todo: invoke lambda function
-
-const insertUrltoDb = async (url: string) => {
+const insertUrltoDb = async (url: string, caption: string) => {
   try {
     await prisma.post.create({
       data: {
         image_url: url,
+        caption: caption,
       },
     });
-    console.log("URLs inserted successfully.");
+    console.log("URL and caption inserted successfully.");
   } catch (error) {
-    console.error("Error inserting URLs:", error);
+    console.error("Error inserting URL and caption:", error);
   } finally {
     await prisma.$disconnect();
   }
@@ -25,9 +24,12 @@ const insertUrltoDb = async (url: string) => {
 export const POST = async (request: Request) => {
   const data = await request.formData();
   const d = data.get("image");
-  if (!d || typeof d === "string") {
-    return new Response("ok", { status: 400 });
+  const caption = data.get("caption");
+
+  if (!d || typeof d === "string" || !caption) {
+    return new Response("Bad Request", { status: 400 });
   }
+
   const stream = await d.arrayBuffer();
   const s3 = new S3Client({
     region: "eu-central-1",
@@ -42,6 +44,12 @@ export const POST = async (request: Request) => {
       ContentType: "image/jpeg",
     })
   );
-  insertUrltoDb(`https://your-bucket-url.amazonaws.com/${key}`);
-  return new Response("ok", { status: 201 });
+
+  const fileContents = Buffer.from(stream).toString("base64");
+
+  const dataUrl = `data:${d.type};base64,${fileContents}`;
+
+  insertUrltoDb(dataUrl, caption.toString());
+
+  return new Response("Created", { status: 201 });
 };
