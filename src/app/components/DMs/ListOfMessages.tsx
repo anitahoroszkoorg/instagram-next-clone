0352a73319673f03294b077fd931190175de99ab
anitahoroszko <anitahoroszko@gmail.com>
2024-01-07 11:42:33 +0100

    minor changes with ui

---
+++ src/app/components/DMs/ListOfMessages.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+ const ListOfMessages = ()=> {
+    return <>
+    <div>XDDD</div>
+    </>
+}
+
+export default ListOfMessages  
--- src/app/pages/followers/page.tsx
+++ src/app/pages/followers/page.tsx
@@ -1,34 +1,12 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from "react";
+import { Followers } from "@/app/components/Followers/Followers"
+import Header from "@/app/components/Header/Header"
 
-export const Followers = () => {
-  const [userData, setUserData] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/test")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setUserData(data);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, []);
+const page = ()=> {
   return (
-    <div>
-      <h1>Instagram User Data</h1>
-      {error ? (
-        <p>Error: {error}</p>
-      ) : (
-        userData && <pre>{JSON.stringify(userData, null, 2)}</pre>
-      )}
-    </div>
-  );
-};
+    <><Header /><Followers /></>
+  )
+}
+
+export default page
