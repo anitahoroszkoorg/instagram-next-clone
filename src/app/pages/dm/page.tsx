a450832314743c4694040a42dda867fab42f413d
anitahoroszko <anitahoroszko@gmail.com>
2024-01-17 21:17:42 +0100

    layout changes

--- src/app/pages/dm/page.tsx
+++ src/app/pages/dm/page.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import DMPage from "../../components/DMs/DMPage";
+import { AppWrapper } from "../../../../styled";
 
 const page = () => {
   return (
     <>
       <Header />
-      <DMPage />
+      <AppWrapper>
+        <DMPage />
+      </AppWrapper>
     </>
   );
 };
