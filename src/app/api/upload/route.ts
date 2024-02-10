74642be6ac9df4403a33834be0e4f4c60f00c2d9
anitahoroszko <anitahoroszko@gmail.com>
2024-02-10 12:46:49 +0100

    previous solution to POST to s3

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,7 +2,6 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import { PrismaClient } from "@prisma/client";
-import { Readable } from "stream";
 
 const prisma = new PrismaClient();
 
@@ -26,27 +25,26 @@ export const POST = async (request: Request) => {
   const data = await request.formData();
   const d = data.get("image");
   const caption = data.get("caption");
+
   if (!d || typeof d === "string" || !caption) {
     return new Response("Bad Request", { status: 400 });
   }
+
   const stream = await d.arrayBuffer();
-  const readableStream = new Readable();
-  readableStream.push(Buffer.from(stream));
-  readableStream.push(null);
   const s3 = new S3Client({
     region: "eu-central-1",
   });
   const fileType = d.type.split("/").pop();
   const key = `${uuidv4()}.${fileType}`;
+  console.log(stream);
   const results = await s3.send(
     new PutObjectCommand({
-      Body: readableStream,
+      Body: stream,
       Bucket: "aws-bucket-next-ig",
       Key: key,
       ContentType: "image/jpeg",
     }),
   );
-  console.log(results);
 
   const fileContents = Buffer.from(stream).toString("base64");
 
