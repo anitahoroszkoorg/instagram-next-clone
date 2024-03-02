389710315e1876aaf7cffe96dff95694ea76a27e
anitahoroszko <anitahoroszko@gmail.com>
2024-03-02 23:07:55 +0100

    created validation for upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,18 +1,11 @@
 import { addPost } from "@/app/services/addPost";
-
+import { validateUploadPostData } from "@/app/schemas/uploadPostSchema";
 export const POST = async (request: Request) => {
-  const data = await request.formData();
-  // Use library to validate
-  const blob = data.get("image");
-  const caption = data.get("caption");
-  if (typeof blob === "string" || !blob) {
-    return new Response("Bad Request", { status: 400 });
+  const formData = await request.formData();
+  const { error, values } = validateUploadPostData(formData);
+  if (error) {
+    return new Response(error, { status: 400 });
   }
-  if (typeof caption !== "string" || !blob) {
-    return new Response("Bad Request", { status: 400 });
-  }
-  //
-  addPost(blob, caption);
-
-  return new Response("Created", { status: 201 });
+  await addPost(values.image as File, values.caption as string);
+  return new Response(JSON.stringify(values), { status: 200 });
 };
---
+++ src/app/schemas/uploadPostSchema.ts
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+export const uploadPostSchema = Joi.object({
+  image: Joi.object().instance(File),
+  caption: Joi.string().required().min(3).max(255),
+});
+
+export const imageSchema = Joi.object({
+  type: Joi.string().valid("image/png", "image/jpeg").required(),
+  size: Joi.required(),
+});
+
+export const validateUploadPostData = (formData: FormData) => {
+  const data = Object.fromEntries(formData.entries());
+  const { error: uploadPostError, values } = uploadPostSchema.validate(data);
+  const image = values.image;
+  const { error: imageError, _ } = imageSchema.validate({
+    size: image.size,
+    type: image.type,
+  });
+  const error = uploadPostError || imageError;
+  return { error, values };
+};
