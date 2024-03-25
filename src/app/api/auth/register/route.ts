e5631cd184bd7b8c8c7c8a11979678350360f0f8
anitahoroszko <anitahoroszko@gmail.com>
2024-03-25 21:10:10 +0100

    work in progress

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,37 +4,64 @@ import { addUser } from "@/app/db/users";
 import { NewUser } from "@/globals";
 import { validateAddUserData } from "@/app/schemas/addUserSchema";
 import { PrismaClient } from "@prisma/client";
+const nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASS,
+  },
+  secure: true,
+});
 
 const prisma = new PrismaClient();
-
-//when user tries to register first check if he exists in the database, if exists give notification to log in instead, if the user doesn't exist send him email to verify registration
 
 export async function POST(request: Request) {
   try {
     const { email, password, username, full_name } = await request.json();
-    const checkExisitngUser = await prisma.instagram_user.findFirst({
+    const checkExistingUser = await prisma.instagram_user.findFirst({
       where: {
-        email: email,
+        OR: [
+          {
+            email: email,
+          },
+          {
+            username: username,
+          },
+        ],
       },
     });
-    if (checkExisitngUser) {
+    if (checkExistingUser) {
       return NextResponse.redirect("/pages/login");
-    }
-    const hashedPassword = await hash(password, 10);
-    const user: NewUser = {
-      email: email,
-      password_hash: hashedPassword,
-      username: username,
-      full_name: full_name,
-    };
-    const { error, values } = validateAddUserData(user);
-    if (error) {
-      console.error(error);
     } else {
-      addUser(values);
+      const info = await transporter.sendMail({
+        from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
+        to: email,
+        subject: "Hello ✔",
+        text: "Hello world?",
+        html: "<b>Hello world?</b>",
+      });
+      console.log("Message sent: %s", info.messageId);
+
+      const hashedPassword = await hash(password, 10);
+      const user: NewUser = {
+        email: email,
+        password_hash: hashedPassword,
+        username: username,
+        full_name: full_name,
+      };
+
+      const { error, values } = validateAddUserData(user);
+      if (error) {
+        console.error(error);
+      } else {
+        addUser(values);
+      }
     }
   } catch (e) {
-    console.log({ e });
+    console.error("Error:", e);
   }
   return NextResponse.json({ message: "success" });
 }
