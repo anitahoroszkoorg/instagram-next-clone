db4d9e1d3a83c50f07fa60f39352a90f035cd893
janosiecki <jan.osiecki>
2025-01-12 21:45:15 +0100

    fix activate flow

---
+++ src/app/db/errors.ts
@@ -0,0 +1,14 @@
+export class UserIsAlreadyActiveError extends Error {
+    constructor(message: string) {
+        super(message); 
+        this.name = "UserIsAlreadyActiveError"; 
+        Object.setPrototypeOf(this, UserIsAlreadyActiveError.prototype);
+    }
+}
+export class UserDoesntExistError extends Error {
+    constructor(message: string) {
+        super(message); 
+        this.name = "UserDoesntExistError"; 
+        Object.setPrototypeOf(this, UserDoesntExistError.prototype);
+    }
+}
