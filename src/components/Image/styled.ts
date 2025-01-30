d377ef0e519a9ac435dc2a8ddbb987c3df55b7bc
anitahoroszko <anitahoroszko@gmail.com>
2025-01-30 21:53:55 +0100

    style enhancements for post

--- src/components/Image/styled.ts
+++ src/components/Image/styled.ts
@@ -10,7 +10,7 @@ export const Photobox = styled.img`
 export const PhotoDescription = styled.p`
   text-align: justify;
   text-justify: inter-word;
-  font-size: 0.8rem;
+  font-size: 14px;
   margin: 0.5rem;
 `;
 
@@ -23,7 +23,7 @@ export const BackDropContainer = styled.div<{ visible: boolean }>`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: var(--ig-backdrop);
   z-index: 1000;
 `;
 
@@ -45,7 +45,7 @@ export const Photo = styled.img.attrs((props) => ({
   width: 100%;
   height: 400px;
   object-fit: cover;
-  background: #f4f4f4;
+  background: var(--ig-medium-grey);
 `;
 
 export const PhotoDetails = styled.div`
@@ -62,24 +62,23 @@ export const Avatar = styled.img`
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  background: #ccc;
+  background: var(--ig-background-default);
   margin-right: 0.5rem;
 `;
 
 export const Username = styled.span`
   font-weight: bold;
-  color: #333;
+  color: var(--ig-dark-blue);
 `;
 
 export const Caption = styled.p`
-  font-size: 1rem;
-  color: #555;
+  font-size: 14px;
+  color: var(--ig-black);
   margin: 0.5rem 0;
 `;
 
 export const TagsContainer = styled.div`
-  font-size: 0.9rem;
-  color: #007aff;
+  font-size: 14px;
   margin-bottom: 1rem;
 `;
 
@@ -87,8 +86,7 @@ export const LikeSection = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
-  font-size: 1rem;
-  color: #333;
+  font-size: 14px;
   margin-bottom: 1rem;
 `;
 
@@ -99,17 +97,18 @@ export const Icon = styled.div`
 `;
 
 export const CommentsSection = styled.div`
-  font-size: 0.9rem;
-  color: #999;
+  font-size: 14px;
 `;
 
 export const CommentItem = styled.div`
-  background-color: #f1f1f1;
-  padding: 8px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
   margin-bottom: 8px;
   border-radius: 4px;
   font-size: 14px;
-  color: #333;
+  color: var(--ig-black);
+  width: 100%;
 `;
 
 export const CommentsInputContainer = styled.div`
@@ -122,29 +121,28 @@ export const CommentsInputContainer = styled.div`
 export const StyledButton = styled.button`
   padding: 6px 12px;
   font-size: 14px;
-  background-color: #007aff;
-  color: #fff;
+  background-color: var(--ig-blue);
+  color: var(--ig-black);
   border: none;
   border-radius: 4px;
   cursor: pointer;
   align-self: flex-end;
-  &:hover {
-    background-color: #005bb5;
-  }
 `;
+
 export const Input = styled.input`
-  width: 90%;
+  width: 100%;
   height: 30px;
   padding: 10px;
   border: none;
   border-bottom: 1px solid #ccc;
-  font-size: 16px;
-  font-weight: 300;
+  font-size: 14px;
   &:focus {
     outline: none;
-    border-bottom: 2px solid #2196f3;
+    border-bottom: 1px solid var(--ig-dark-blue);
   }
-  margin: 10px;
+  background-color: var(--ig-divider-grey);
+  border-radius: 4px;
+  color: var(--ig-black);
 `;
 
 export const Button = styled.button`
@@ -152,17 +150,17 @@ export const Button = styled.button`
   border: none;
   height: 45px;
   border-radius: 3px;
-  background-color: #2196f3;
+  background-color: var(--ig-blue);
   color: white;
-  font-size: 16px;
-  font-weight: 300;
-  cursor: pointer;
-  &:hover {
-    background-color: #0d8bf2;
-  }
+  font-size: 14px;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   margin-top: 30px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const StyledForm = styled.form`
+  width: 100%;
 `;
