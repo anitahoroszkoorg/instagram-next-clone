c351f6840617b61191fe6a6e13a7ecb63b026323
anitahoroszko <anitahoroszko@gmail.com>
2025-02-22 15:51:27 +0100

    explore page styles

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -1,31 +1,42 @@
+"use client";
 import useFetch from "@/app/lib/hooks/useFetch";
 import { PostDetails } from "@/shared/types/post";
 import { useState } from "react";
-import { FeedWrapper, Photobox } from "./styled";
+import {
+  FeedWrapper,
+  Photobox,
+  ContentContainer,
+  SearchContainer,
+  ExploreContainer,
+} from "./styled";
 
 export const Explore = () => {
   const { data, loading, error } = useFetch<{ posts: PostDetails[] }>(
     "/api/post",
   );
-  console.log("Data from API:", data);
   const [selectedImage, setSelectedImage] = useState<PostDetails | null>(null);
 
   return (
     <>
-      <FeedWrapper>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        {!!data && data.posts.length > 0
-          ? data.posts.map((image) => (
-              <Photobox
-                key={image.post_id}
-                src={image.image}
-                alt="photo"
-                onClick={() => setSelectedImage(image)}
-              />
-            ))
-          : !loading && <p>No posts available</p>}
-      </FeedWrapper>
+      <ExploreContainer>
+        <SearchContainer></SearchContainer>
+        <ContentContainer>
+          <FeedWrapper>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {!!data && data.posts.length > 0
+              ? data.posts.map((image) => (
+                  <Photobox
+                    key={image.post_id}
+                    src={image.image}
+                    alt="photo"
+                    onClick={() => setSelectedImage(image)}
+                  />
+                ))
+              : !loading && <p>No posts available</p>}
+          </FeedWrapper>
+        </ContentContainer>
+      </ExploreContainer>
     </>
   );
 };
