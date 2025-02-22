762cdf4cf62a174b2c2d966e39682e24174ea983
anitahoroszko <anitahoroszko@gmail.com>
2025-02-22 15:52:29 +0100

    explore page styles

--- src/components/Explore/styled.ts
+++ src/components/Explore/styled.ts
@@ -1,39 +1,42 @@
 import styled from "styled-components";
-import { keyframes } from "styled-components";
+
+export const ExploreContainer = styled.div`
+  display: flex;
+  margin-top: 15vh;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 1;
+`;
 
 export const FeedWrapper = styled.div`
   display: flex;
   width: 100%;
-  height: 80%;
+  height: 100%;
   flex-wrap: wrap;
   overflow-x: hidden;
-  margin-top: 15vh;
+  justify-content: center;
+  margin-bottom: 10vh;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
   justify-content: center;
 `;
 
-const myAnim = keyframes`
-  0% {
-    transform: scale(1);
-  }
-  70% {
-    transform: scale(1.01);
-  }
-  100% {
-    transform: scale(1);
-  }
-`;
-
 export const Photobox = styled.img`
-  margin: 2rem;
-  width: 15em;
-  height: 15em;
+  margin: 0rem 1rem 1rem 1rem;
+  width: 25em;
+  height: 25em;
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
   overflow: hidden;
   border-radius: 10px;
   display: flex;
-  &:hover {
-    animation: ${myAnim} 1.2s ease-in-out;
-  }
 `;
 
 export const ModalContent = styled.div`
@@ -117,4 +120,12 @@ export const Caption = styled.div`
   margin-top: 20px;
   font-size: 16px;
   text-align: center;
+`;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 40%;
+  justify-content: center;
+  padding: 0em 1em;
 `;
